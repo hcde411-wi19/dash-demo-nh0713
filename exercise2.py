34875308a5c3ba2925f73d96ae2d3a1c480f44ec
Nathan Han <nathan0713@gmail.com>
2019-03-07 18:58:27 -0800

    'exercises'

--- exercise2.py
+++ exercise2.py
@@ -2,8 +2,43 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
+app = dash.Dash(__name__, static_folder='static')
+df = pd.read_csv('static/data_car_2004.csv')
+app.layout = html.Div(children=[
 
-# Exercise 2
-# Create another visualization of your choice of data and chart type.
-# You can use pandas to help loading data, or just hard-coded the data is fine.
+    html.H1(children='Scatter Plot'),
+
+    html.Div(children='''
+        A demo to show a scatter plot.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df['HP'],
+                    y=df['Retail Price'],
+                    mode='markers',
+                    text=df['Vehicle Name'],
+                    marker={
+                        'size': 10,
+                        'opacity': 0.8
+                    }
+                )
+            ],
+            'layout': {
+                'title': 'Car Dataset 2004',
+                'xaxis': {'title': 'Horse Power'},
+                'yaxis': {'title': 'Retail Price'},
+            }
+        }
+    )
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
